548da3d58f31ae7aded5b673800b56229e76a683
OiC-b8024-Inoue <49602459+KoreanGinseng@users.noreply.github.com>
2021-09-18 08:07:52 +0900

    add projects

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 
 使用ライブラリ：DxLib for Android https://dxlib.xsrv.jp/DxLib/DxLib_Android3_22c.zip
 
+​	　　　DX Library Copyright (C) 2001-2019 Takumi Yamada.
+
 使用ツール：android studio
 
---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,20 @@
+#include <DxLib.h>
+
+// プログラムは android_main から始まります
+int android_main( void )
+{
+    if ( DxLib_Init() == -1 )		// ＤＸライブラリ初期化処理
+    {
+
+        return -1 ;			// エラーが起きたら直ちに終了
+    }
+    // メッセージループに代わる処理をする
+    while ( ProcessMessage() != -1 )
+    {
+        DrawBox( 220, 140, 420, 340, GetColor( 255,255,255 ), TRUE ) ;	// 四角形を描画する
+    }
+
+    DxLib_End() ;				// ＤＸライブラリ使用の終了処理
+
+    return 0 ;					// ソフトの終了
+}
