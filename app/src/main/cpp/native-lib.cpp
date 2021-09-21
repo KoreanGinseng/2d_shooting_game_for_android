a28a5529c5856d60b35847ba25c7514379863b3e
OiC-b8024-Inoue <49602459+KoreanGinseng@users.noreply.github.com>
2021-09-21 10:16:27 +0900

    change project

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,16 @@
-#include <DxLib.h>
+#include "DxLib.h"
 
 // プログラムは android_main から始まります
 int android_main( void )
 {
-    if ( DxLib_Init() == -1 )		// ＤＸライブラリ初期化処理
+    if( DxLib_Init() == -1 )		// ＤＸライブラリ初期化処理
     {
-
         return -1 ;			// エラーが起きたら直ちに終了
     }
-    // メッセージループに代わる処理をする
-    while ( ProcessMessage() != -1 )
-    {
-        DrawBox( 220, 140, 420, 340, GetColor( 255,255,255 ), TRUE ) ;	// 四角形を描画する
-    }
+
+    DrawBox( 220, 140, 420, 340, GetColor( 255,255,255 ), TRUE ) ;	// 四角形を描画する
+
+    WaitKey() ;				// キー入力待ち
 
     DxLib_End() ;				// ＤＸライブラリ使用の終了処理
 
